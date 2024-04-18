1937af3bb1dad98bb6f8541f57868290774b4d84
Sameemul Haque <110324374+sameemul-haque@users.noreply.github.com>
2024-04-18 13:48:43 +0530

    fix: update database
    * fix: update database
    
    * update index_name
    
    Co-authored-by: Nuzaim Noushad Thappi <85758853+Nuzaim@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nuzaim Noushad Thappi <85758853+Nuzaim@users.noreply.github.com>

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def main():
 
     # connect to mongodb
     client = pymongo.MongoClient(mongodb_connection_string)
-    db = client.test_database
+    db = client.database
     collection = db.textbooks
 
     query = request.args.get('q')
@@ -62,17 +62,15 @@ def main():
 
     vector_search = MongoDBAtlasVectorSearch.from_connection_string(
         mongodb_connection_string,
-        "test_database" + "." + "textbooks",
+        "database" + "." + "textbooks",
         instructor_embeddings,
-        index_name="default",
+        index_name="search",
     )
     retriever = vector_search.as_retriever(
         search_type="similarity",
         search_kwargs={"k": 3},
     )
-    
 
-   
     # prompt template
     prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
